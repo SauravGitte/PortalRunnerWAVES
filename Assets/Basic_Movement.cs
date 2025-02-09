cf8f133813193942098d1b5daecc1619465d8511
Atharv Badgujar <atharv23m@gmail.com>
2025-02-10 02:22:40 +0530

    added path and player

---
+++ Assets/Basic_Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Dreamteck.Forever;
+
+public class Basic_Movement : MonoBehaviour
+{
+    public float jumpForce = 10f, moveSpeed = 5;
+    private Runner runner;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        runner = GetComponent<Runner>();
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = runner.motion.offset;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        pos.x += horizontalInput * moveSpeed * Time.deltaTime;
+        runner.motion.offset.Set(pos.x, pos.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
