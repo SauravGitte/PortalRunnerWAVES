84c14ece7d643e16490c61314f2dd1c03ab534a0
Atharv Badgujar <atharv23m@gmail.com>
2025-02-16 19:10:34 +0530

    calamity

--- Assets/Scripts/Calamity.cs
+++ Assets/Scripts/Calamity.cs
@@ -1,13 +1,45 @@
+using Dreamteck.Forever;
+using System.Collections;
 using UnityEngine;
 
 public class Calamity : MonoBehaviour
 {
+    public float minSpeed = 5f;
+    public float maxSpeed = 35f;
+    public float speedIncreaseDuration = 10f; // Time to reach max speed
+    private Runner runner;
+    private float currentSpeed;
+    private float speedIncrement;
+
+    private void Start()
+    {
+        runner = GetComponentInParent<Runner>();
+        currentSpeed = minSpeed;
+        speedIncrement = (maxSpeed - minSpeed) / speedIncreaseDuration;
+        StartCoroutine(IncreaseSpeed());
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Cursor.visible = false;
+    }
+
+    private IEnumerator IncreaseSpeed()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < speedIncreaseDuration)
+        {
+            currentSpeed += speedIncrement * Time.deltaTime;
+            runner.followSpeed = Mathf.Clamp(currentSpeed, minSpeed, maxSpeed);
+            yield return null;
+            elapsedTime += Time.deltaTime;
+        }
+        runner.followSpeed = maxSpeed; // Ensure it caps at max speed
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided with " + other.gameObject.name);
         if (other.gameObject.CompareTag("Player"))
         {
-
             Debug.Log("Game Over");
             Time.timeScale = 0f;
         }
